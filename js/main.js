a12d9d6e91aeebfa0d45e3268302b0c6e3448d7b
yinia <yinia0608@163.com>
2014-11-27 15:08:59 +0800

    y2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 ﻿$(function(){
+	//初始时，隐藏选套餐
+	$("#selectfood").hide();
+	
 	//加载餐厅列表
 	$.getJSON("data/restaurant.json",function(data){
 		var lis = "";
@@ -26,15 +29,19 @@
 		}
 		//赋值给餐厅input
 		$("#restaurant").val(restauran);
-		//餐厅值不为空，加载该餐厅的套餐列表
+		//餐厅值不为空
 		if(restauran!=""){
+			//显示选套餐
+			$("#selectfood").show();
+			//加载该餐厅的套餐列表
 			$.getJSON("data/food.json", function(data){	
 				var lis = "";
 				$.each(data, function(i,item){
 					if(item.restaurant == restauran){
 						$.each(item.food, function(i,item_food){ 
 							//alert(item_food.food_name)
-							var one_li = "<li><a href='#' data-rel='back' data-value='"+item_food.food_name+"'>"+item_food.food_name+"</a></li>"
+							var one_li = "<li><a href='#' data-rel='back' data-value='"+item_food.food_name+","+item_food.price+"'>"+
+										item_food.food_name+"<span class='ui-li-count'>￥"+item_food.price+"</span></a></li>"
 							//alert(one_li);
 							lis = lis + one_li;	
 						}); 
@@ -43,16 +50,66 @@
 				$("#food_list").html(lis);
 				$("#food_list").listview("refresh");
 			});
-		}
-		
+		}		
      });
 	 
 	 //选套餐 点击选中 onclick事件
 	 $("#food_list").on("click", "a[data-value]", function(){
 		var food = $(this).attr("data-value");
-		$("#food").val(food);
+		food = food.split(',');
+		$("#food").val(food[0]);
+		$("#price").val(food[1]);
 	 });
+	 
+	 //提交表单	 
+	 $("#submitbtn").click(function(){
+		var username = $("#username").val();
+		var restaurant = $("#restaurant").val();
+		var food = $("#food").val();	
+		var price = $("#price").val();
+		if(username!="" && restaurant!="" && food!="" && price!=""){
+			//保存订单信息
+			var data = {"username":username,"restaurant":restaurant,"food":food,"price":price};
+			$.jStorage.set("order"+username, data);
+			$.mobile.changePage("order.html#pagesuccess");
+			//清空表单
+			$("#username").val("");
+			$("#restaurant").val("");
+			$("#food").val("");
+			$("#price").val("");
+			$("#selectfood").hide();
+		}else{
+			//alert("信息填写不完整！");			
+			$.mobile.changePage("order.html#pageerror");		
+		}
+	 });
+	 
+	//加载订单
+	getOrders();
+	
+	//清空订单
+	$("#clearbtn").click(function(){
+		$.jStorage.flush();
+		location.reload();
+	});
 });
+
+function getOrders(){	
+	var orderlist = $.jStorage.index();
+	//alert(orderlist.length+"--"+orderlist[0]+"--"+orderlist[1]);	
+	var lis = "";
+	var totalprice = 0;
+	for(var i=0; i<orderlist.length; i++){
+		var one_order = $.jStorage.get(orderlist[i]);
+		var one_li = "<li data-role='list-divider'>"+one_order.username+"</li>"+
+					"<li data-icon='false'><a href='#'><span class='ui-li-count'>￥"+one_order.price+"</span><h2>"+one_order.restaurant+"</h2><p>"+one_order.food+"</p></a></li>";
+		lis = lis + one_li;
+		totalprice = (parseFloat(totalprice) + parseFloat(one_order.price)).toFixed(1);
+	}
+	lis = "<li data-role='divider'>共"+orderlist.length+"份订单，合计"+totalprice+"元</li>"+lis;
+	$("#vieworder").html(lis);
+	$("#vieworder").listview("refresh");
+}
 
 
 
